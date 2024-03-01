d8fb7bd463343c8ca7995f2e5d58dc3605cf7117
Juan Sebastian Gomez Ayala <j.gomezayala@globant.com>
2024-03-01 15:03:34 -0500

    proyecto base

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client';
+import FormScreen from './pages/FormScreen'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <FormScreen />
+  </React.StrictMode>,
+)
---
+++ src/pages/FormScreen.jsx
@@ -0,0 +1,9 @@
+
+
+export default function FormScreen() {
+    return (
+        <>
+        <h1>Form React</h1>
+        </>
+    )
+}
